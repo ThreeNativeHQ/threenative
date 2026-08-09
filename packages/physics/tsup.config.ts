e7efa69daedb78434fc884b9e4e118f594dba1d4
Joao Paulo Furtado <joaopaulofurtado@live.com>
2026-08-09 00:18:04 -0700

    style(physics): format build entries

--- packages/physics/tsup.config.ts
+++ packages/physics/tsup.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/navigation/index.ts",
-    "src/proof.ts",
-    "src/native/index.ts",
-    "src/native/proof.ts",
-  ],
+  entry: ["src/index.ts", "src/navigation/index.ts", "src/native/index.ts"],
   format: ["esm"],
   target: "es2022",
   dts: true,
